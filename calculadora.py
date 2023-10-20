de3d824229ca1bdcf1d146ce684990749fd65c35
liliasoto <0.lilia.soto.0@gmail.com>
2023-10-20 11:50:26 -0600

    Modified calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,11 +1,19 @@
-import argparse
+# Función principal para la calculadora
+def calculadora():
+    print("Calculadora básica")
+    while True:
+        print("Opciones:")
+        print("1. Sumar")
+        print("2. Restar")
+        print("3. Multiplicar")
+        print("4. Dividir")
+        print("5. Salir")
+        
+        opcion = input("Elige una opción (1/2/3/4/5): ")
+        
+        if opcion == '5':
+            break
 
-def calcular(operacion, num1, num2):
-    
 
-def main():
-    
-
-if __name__ == '__main__':
-    main()
-
+if _name_ == "_main_":
+    calculadora()
