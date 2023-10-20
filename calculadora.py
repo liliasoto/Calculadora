9b71e08a835f5d886cb1a2805180bb893aa37875
Ramses <20460181@colima.tecnm.mx>
2023-10-20 12:58:08 -0500

    modified calculadora.py try-catch

--- calculadora.py
+++ calculadora.py
@@ -13,6 +13,26 @@ def calculadora():
         
         if opcion == '5':
             break
+        try:
+            num1 = float(input("Ingresa el primer número: "))
+            num2 = float(input("Ingresa el segundo número: "))
+        except ValueError:
+            print("Error: Ingresa números válidos.")
+            continue
+
+        if opcion == '1':
+            resultado = suma(num1, num2)
+        elif opcion == '2':
+            resultado = resta(num1, num2)
+        elif opcion == '3':
+            resultado = multiplicacion(num1, num2)
+        elif opcion == '4':
+            resultado = division(num1, num2)
+        else:
+            print("Opción no válida.")
+            continue
+
+        print("Resultado: ", resultado)
 
 
 if _name_ == "_main_":
